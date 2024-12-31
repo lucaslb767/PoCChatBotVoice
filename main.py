c9983e0d018e41021f1f92507510ec8815c8169c
LucasB <lucaslbarrozo@gmail.com>
2024-12-31 18:50:10 -0300

    finishes bot

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import speech_recognition as sr
 import openai
 from dotenv import load_dotenv, find_dotenv
 from io import BytesIO
+import playsound
+from pathlib import Path
 
 _ = load_dotenv(find_dotenv())
 client = openai.Client()
 
 recognizer = sr.Recognizer()
-
+AUDIO_FILE = 'Assitant_voice.mp3'
 def record_audio():
     with sr.Microphone() as source:
         print('Listening...')
@@ -25,7 +27,7 @@ def audio_to_text(audio):
 
     return transcription.text
 
-def ai_answer(messages):
+def ai_answer_text(messages):
     answer = client.chat.completions.create(
         messages=messages,
         model='gpt-4o-mini',
@@ -33,6 +35,20 @@ def ai_answer(messages):
         temperature=0
     )
     return answer
+
+def ai_answer_audio(text):
+    if Path(AUDIO_FILE).exists():
+        Path(AUDIO_FILE).unlink()
+    answer = client.audio.speech.create(
+        model='tts-1',
+        voice='alloy',
+        input=text
+    )
+    answer.write_to_file(AUDIO_FILE)
+
+def play_audio():
+    
+    playsound.playsound(AUDIO_FILE)
 
 if __name__ == '__main__':
     messages = []
@@ -42,6 +58,8 @@ if __name__ == '__main__':
         transcription = audio_to_text(audio)
         messages.append({'role': 'user', 'content': transcription})
         print(f'User: {messages[-1]['content']}')
-        answer = ai_answer(messages)
+        answer = ai_answer_text(messages)
         messages.append({'role': 'assistant', 'content': answer.choices[0].message.content})
         print(f'Bot: {messages[-1]['content']}')
+        ai_answer_audio(messages[-1]['content'])
+        play_audio()
