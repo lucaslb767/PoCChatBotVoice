9170d2a644fa5ca7a0fe182037f727114a625616
LucasB <lucaslbarrozo@gmail.com>
2024-12-31 17:46:53 -0300

    transcribe your voice data to text

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import openai
 from dotenv import load_dotenv, find_dotenv
+from io import BytesIO
 
 _ = load_dotenv(find_dotenv())
 client = openai.Client()
@@ -14,7 +15,18 @@ def record_audio():
         audio = recognizer.listen(source)
     return audio
 
+def audio_to_text(audio):
+    wav_data = BytesIO(audio.get_wav_data())
+    wav_data.name = 'audio.wav'
+    transcription = client.audio.transcriptions.create(
+        model='whisper-1',
+        file=wav_data,
+    )
+
+    return transcription.text
+
+
 if __name__ == '__main__':
     audio = record_audio()
-    with open('test_audio.wav', 'wb') as ad:
-        ad.write(audio.get_wav_data())
+    transcription = audio_to_text(audio)
+    print(transcription)
