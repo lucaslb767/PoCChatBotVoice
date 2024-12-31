4a3a9fa1b3e1608942ab790f35c970923086f3bf
LucasB <lucaslbarrozo@gmail.com>
2024-12-31 17:15:19 -0300

    creates function to listen and record audio from mic

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
 client = openai.Client()
 
+recognizer = sr.Recognizer()
+
+def record_audio():
+    with sr.Microphone() as source:
+        print('Listening...')
+        recognizer.adjust_for_ambient_noise(source, duration=1)
+        audio = recognizer.listen(source)
+    return audio
 
